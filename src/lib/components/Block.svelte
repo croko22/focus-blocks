1f7fb7736dadad1383a064e6e13096a7b8f05dc9
croko22 <kev132011@hotmail.com>
2023-06-09 11:43:50 -0500

    f: Block that renders height according the time, (to be finished)

---
+++ src/lib/components/Block.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let name = '';
+	export let time = 0;
+
+	let blockHeight = time / 10 + 'px'; // Adjust the multiplier as needed
+
+	function updateTime(event) : void {
+		time = parseInt(event.target.textContent);
+        if (isNaN(time)) {
+            time = 0;
+        }
+		blockHeight = `${time / 10}px`; // Adjust the multiplier as needed
+	}
+</script>
+
+	<div class="block" style="--block-height: {blockHeight}">
+		<p>{name}</p>
+		<p contenteditable="true" class="time" on:input={updateTime} bind:textContent={time}>
+			{time}
+		</p>
+	</div>
+
+<style>
+	.block {
+		width: 100%;
+		background-color: blue;
+		height: var(--block-height, 100px); /* Default height */
+		transition: height 0.3s ease;
+	}
+</style>
