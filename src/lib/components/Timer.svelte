d7c0a0120e2d456c74c0f4c3f9a9c9061d3414a0
croko22 <kev132011@hotmail.com>
2023-06-09 10:52:16 -0500

    c: Starting again, now with more xp

---
+++ src/lib/components/Timer.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte';
+  
+    let time = 0;
+    let timerInterval: number;
+    let isRunning = false;
+    let setTime = 25 * 60; // Initial time in seconds (25 minutes)
+  
+    const formatTime = (time: number) => {
+      const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+      const seconds = (time % 60).toString().padStart(2, '0');
+      return `${minutes}:${seconds}`;
+    };
+  
+    const startTimer = () => {
+      isRunning = true;
+      timerInterval = setInterval(() => {
+        if (time > 0) {
+          time--;
+        } else {
+          stopTimer();
+          alert('Time is up!');
+        }
+      }, 1000);
+    };
+  
+    const stopTimer = () => {
+      isRunning = false;
+      clearInterval(timerInterval);
+    };
+  
+    const resetTimer = () => {
+      stopTimer();
+      time = setTime;
+    };
+  
+    onMount(() => {
+      time = setTime;
+    });
+  
+    onDestroy(() => {
+      stopTimer();
+    });
+  </script>
+  
+  <style>
+    .time-label {
+      @apply flex items-center justify-center rounded-full h-32 w-32 text-4xl font-bold bg-blue-500 text-white;
+    }
+  </style>
+
+  <div class="flex flex-col items-center justify-center h-screen gap-5">
+    <h1 class="h1">Timer</h1>
+    <div class="card p-4">
+      <input  class="bg-transparent rounded-lg font-bold text-lg" type="number" bind:value={setTime} min="1" step="1" />
+      <button class="btn variant-filled" on:click={resetTimer}>Set Time</button>
+    </div>
+    <div class="time-label">{formatTime(time)}</div>
+    <div>
+      {#if !isRunning}
+        <button class="btn variant-filled" on:click={startTimer}>Start</button>
+      {:else}
+        <button class="btn variant-filled-secondary" on:click={stopTimer}>Stop</button>
+      {/if}
+    </div>
+  </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Timer from '$lib/components/Timer.svelte';
+
+	let block = {
+		name: '',
+		time: 0
+	}
+
+	let savedBlock = JSON.parse(localStorage.getItem('block') || '{}');
+
+	const saveToLocal = () => {
+		localStorage.setItem('block', JSON.stringify(block));
+		savedBlock = JSON.parse(localStorage.getItem('block') || '{}');
+	}
+</script>
+
+<div class="container h-full mx-auto flex justify-evenly items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Focus blocks</h1>
+		<input class="bg-transparent rounded-md" type="text" placeholder="Focus block name" bind:value={block.name}>
+		<input class="bg-transparent rounded-md" type="number" placeholder="25" bind:value={block.time}>
+		<button class="btn variant-filled rounded-md" on:click={saveToLocal}>Save block</button>		
+		
+		<!-- * Saved blocks -->
+		<div class="space-y-5">
+			<h1 class="h1">Saved block</h1>
+			<p>{savedBlock.name}</p>
+			<p>{savedBlock.time}</p>
+		</div>
+	</div>
+
+
+
+	<Timer />
+</div>
